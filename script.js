25c9f7ff83226bb3dc713ea6261b41417ca7a75a
Kenan Ibrović <kenan.ibrovic94@gmail.com>
2017-03-24 15:56:45 +0100

    understood weather api

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
-	var x = document.getElementById("demo");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
-}
+
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=3191281&APPID=55b74a747d2c01d818cecf1e258969ee",function(json){
+        	$("#data").html(JSON.stringify(json));
+        	$("#location").html(json.name);
+        	$("#icon").html("<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+        });
+	
+  
 });
