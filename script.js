4f48adc99eac6e2a16ef06c54d32a5b2dc782125
Kenan Ibrović <kenan.ibrovic94@gmail.com>
2017-07-13 16:40:39 +0200

    new-weather-api

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 $(document).ready(function(){
-	var units="metric";
+	var units;
 	var unit_type="°C";
         var lat;
         var lng;
 
-        // Open Weather Maps API Key: 55b74a747d2c01d818cecf1e258969ee
+        // Apixu Weather API 
 
         function getWeather(){
-                $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&units="+units+"&APPID=55b74a747d2c01d818cecf1e258969ee",function(json){
+              
+                $.getJSON("https://api.apixu.com/v1/current.json?key=f2334b1c9d3142f3b0a152818171207&q="+lat+","+lng,function(json){
+                        if (unit_type=="°C"){
+                                units=json.current.temp_c;
+                        } else {
+                                units=json.current.temp_f;
+                        }
                         //$("#location").html(JSON.stringify(json)); //used for testing if api works
-                        $("#location").html(json.name + "," + json.sys.country);
-                        $("#weather").html(json.weather[0].main);
-                        $("#temperature").html(json.main.temp + unit_type);
-                        $("#icon").html("<img src='http://openweathermap.org/img/w/" + json.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
-                });
+                        $("#location").html(json.location.name + "," + json.location.country);
+                        $("#weather").html(json.current.condition.text);
+                        $("#temperature").html(units+unit_type);
+                        $("#icon").html("<img src='https:"+json.current.condition.icon+"' alt='Icon depicting current weather.'>");
+                });  
         }
 
-        // Google Maps Geolocation API key: AIzaSyBFxbekNAX5NeO23YcWbL3teg17ZgV4qFQ
+        // Google Maps Geolocation API 
 
         $.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBFxbekNAX5NeO23YcWbL3teg17ZgV4qFQ", function(geo){
-                //$("#loc").html(JSON.stringify(geo)); TEST IF API WORKS
+                //$("#loc").html(JSON.stringify(geo)); //TEST IF API WORKS
                 lat=geo.location.lat;
                 lng=geo.location.lng;
-                //$("#loc").html("lat= "+lat+" lng= "+lng); ANOTHER TEST
                 getWeather();
         });
 
@@ -30,21 +35,19 @@ $(document).ready(function(){
         getWeather();
 
         $("button").click(function(){
-                if(units=="metric"){
+                if(unit_type==="°C"){
                         $(this).css("-webkit-transform", "translateY(100%)");
-                        units="imperial";
                         unit_type="°F";
                         $("button").html("°F");
                         getWeather();
 
                 } else {
                         $(this).css("-webkit-transform", "translateY(0%)");
-                        units="metric";
                         unit_type="°C";
                         $("button").html("°C");
                         getWeather();
                 }
                 
-        });
+        }); 
         
 });
